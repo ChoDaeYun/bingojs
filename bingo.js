50ebf999f0390667641d32c0bdd891002ad21576
조대연 [Cho <chody0116@neople.co.kr>
2019-05-22 14:46:23 +0900

    bingo js

---
+++ bingo.js
@@ -0,0 +1,63 @@
+;(function($) {
+    var option = {
+        size: 3,
+        list: [],
+        id: 'bingo'
+    }
+    var bingox = {}
+    var bingoy = {}
+    var bingcnt = 0
+    $.bingoJs = {
+        reset:function(){
+            bingox = {}
+            bingoy = {}
+            bingcnt = 0
+            option = {
+                size: 3,
+                list: [],
+                id: 'bingo'
+            }
+        },
+        ready: function(s, id) {
+            this.reset()
+            if (s) option.size = s
+            if (id) option.id = id
+            for (var i = 1; i <= (option.size * option.size); i++) option.list.push(i)
+            this.shuffle()
+            //초기화
+            $("#" + option.id).html("")
+            //틀추가
+            $("#" + option.id).append("<div class='b_table'></div>")
+            var _row = ""
+            for (var i = 0; i < (option.size * option.size); i++) {
+                if (i % option.size === 0) {
+                    _row += "<div class='b_table_row'>"
+                }
+                _row += "<div class='b_table_cell cell_" + i + "' onclick='$.bingoJs.clickcell(" + i + ")' >" + option.list[i] + "</div>";
+                if (i % option.size === (option.size - 1)) {
+                    _row += "</div>"
+                    $(".b_table").append(_row)
+                    _row = ""
+                }
+            }
+        },
+        shuffle: function() {
+            for (var j, x, i = option.list.length; i; j = parseInt(Math.random() * i), x = option.list[--i], option.list[i] = option.list[j], option.list[j] = x);
+        },
+        clickcell: function(s) {
+            var c = s + 1
+            var x = (c % option.size === 0) ? option.size : c % option.size
+            var y = Math.floor((c + option.size - 1) / option.size)
+            if (!bingox[x]) bingox[x] = 0
+            if (!bingoy[y]) bingoy[y] = 0
+            bingox[x]++
+            bingoy[y]++
+            if (bingox[x] == option.size) bingcnt++
+            if (bingoy[y] == option.size) bingcnt++
+            $('.cell_' + s).css("background-color", "red")
+        },
+        chkbingocnt: function() {
+            $(".cnt").html(bingcnt)
+        }
+    }
+})(jQuery);
